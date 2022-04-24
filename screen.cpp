8ec5ef7f327bd59e35dcee9b3e071af2acfa286a
Annabelle Platt <aplatt@olin.edu>
2022-04-24 13:31:27 -0400

    Add the first part of tile rendering for the graphics

--- screen.cpp
+++ screen.cpp
@@ -65,7 +65,7 @@ uint32_t* Screen::get_window() {
 void Screen::render_tiles() {
     uint8_t tile_size_in_memory = 16;
 
-    uint8_t LCD_control = cpu->read_memory(LCDC);
+    uint8_t LCD_control = cpu->read_memory(LCDC); // store the LCD control register
     uint8_t scroll_x = cpu->read_memory(SCX);
     uint8_t scroll_y = cpu->read_memory(SCY);
     uint8_t window_x = cpu->read_memory(WX);
@@ -80,21 +80,49 @@ void Screen::render_tiles() {
         }
     }
 
-
-    uint16_t tile_address = 0;
-    uint8_t tile_x_on_screen = 0; // TODO: figure out where tile is
-    uint8_t tile_y_on_screen = 0;
-
-    if (tileset == 1) {
-        tile_address = 0x8000 + (tile_id)*tile_size_in_memory;
-    } else if (tileset == 0) {
-        tile_address = 0x9000 + (int8_t)tile_id * tile_size_in_memory;
+    uint16_t tile_map_loc;  // this will store the location of the tile in the background map
+    
+    // if there's no window, the background map will be here, otherwise the window will be here
+    if (!window) { 
+        if ((LCD_control & 0x10) >> 4) {
+            tile_map_loc = 0x9C00;
+        } else {
+            tile_map_loc = 0x9800;
+        }
+    } else {
+       if ((LCD_control & 0x2) >> 1) { 
+           tile_map_loc = 0x9C00;
+       } else {
+           tile_map_loc = 0x9800;
+       }
     }
 
-    for (int row_num = 0; row_num < 8; row_num++) {
-        uint8_t row_byte1 = cpu->read_memory(tile_address);
-        uint8_t row_byte2 = cpu->read_memory(tile_address + 1);
+    uint8_t abs_y = cpu->read_memory(LY) + scroll_y; // the absolute y position in the scanline
+    uint16_t tile_row = (abs_y / 8) * 32;  // the vertical tile. TODO: what data structure to use here?
+    
+    uint16_t tile_data_address = 0; // this will store where the actual tile data lives
 
+
+    for (int row_num = 0; row_num < 160; row_num++) {
+        uint8_t abs_x; // TODO: calculate the x position into the va
+        
+        // TODO: calculate tile col, not sure what data structure to use
+        
+        tile_map_loc = tile_map_loc + tile_row + tile_col; // this will store where the tile id lives in the background map
+
+        // TODO read the memory at the location calculated in tile_map_loc to get the tile id
+
+        if ((LCD_control & 0x8) >> 3) {
+            tile_data_address = 0x8000 + (tile_id)*tile_size_in_memory;
+        } else {
+            tile_data_address = 0x9000 + (int8_t)tile_id * tile_size_in_memory;
+        }
+
+        uint8_t row_byte1 = cpu->read_memory(tile_data_address);
+        uint8_t row_byte2 = cpu->read_memory(tile_data_address + 1);
+
+
+        // below this line not refactored, variable names and logic may be wrong
         for (int bit_num = 0; bit_num < 8; bit_num++) {
             uint8_t color = (row_byte1 & 0b10000000) + ((row_byte2 & 0b10000000) << 1);
             set_pixel(color, tile_x_on_screen + bit_num, tile_y_on_screen + row_num);
