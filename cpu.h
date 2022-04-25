a1ba9cea5c0ee78bb5fbd232eddc7a517d72be42
Emma <emack@olin.edu>
2022-04-25 17:00:40 -0400

    Code cleanup related to drawing scanlines and testing graphics

--- cpu.h
+++ cpu.h
@@ -15,12 +15,11 @@ public:
     void set_PC(uint16_t bytes);
     void set_SP(uint16_t bytes);
 
-    int write_memory(uint8_t address, uint8_t data);
-    uint8_t read_memory(uint8_t address);
+    int write_memory(uint16_t address, uint8_t data);
+    uint8_t read_memory(uint16_t address);
 
 private:
     uint8_t memory[0x10000];
     uint16_t PC;
     uint16_t SP;
 };
-
--- graphics.h
+++ graphics.h
@@ -5,6 +5,11 @@
 #include <SDL_error.h>
 #include <SDL_events.h>
 
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 144
+#define SCALE 1
+#define ROW_SIZE sizeof(uint32_t) * SCREEN_WIDTH
+
 void init_graphics(SDL_Window **window_pointer, SDL_Renderer **renderer_pointer,\
     SDL_Texture **texture_pointer, int windowWidth, int windowHeight, \
     int textureWidth, int textureHeight);
@@ -13,3 +18,5 @@ void update_graphics(SDL_Renderer **renderer, SDL_Texture **texture,\
     uint32_t **screen, int rowSize);
 
 void quit_graphics(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture);
+
+int process_input();
